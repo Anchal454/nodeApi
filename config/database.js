16e5463c0089220ff950d7c5359f43b86209cf73
WdevAnchaly <95478519+WdevAnchaly@users.noreply.github.com>
2022-05-20 09:02:18 +0530

    main

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// const url = process.env.DATABASE_URL || 'mongodb://63.142.250.23/dd-development';
+// const url = 'mongodb+srv://anchalyadav:anchalyadav@cluster0.oaset.mongodb.net/?retryWrites=true&w=majority/db1';
+// const url = 'mongodb://103.121.115.132/db1';
+// const url = 'mongodb://127.0.0.1:27017/db1';
+const  url = 'mongodb://localhost:db';
+
+export default {url};
